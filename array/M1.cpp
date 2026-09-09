c95ba16b4822e6b8fab04d021a7b971342dcee7c
vatsal <vatsallsinghal@gmail.com>
2026-09-09 19:02:16 +0530

    m1 twosum1

--- array/M1.cpp
+++ array/M1.cpp
@@ -1,45 +1,103 @@
-// two sum
 #include <iostream>
-
 #include <vector>
-#include <unordered_map>
-
+#include <algorithm>
+// two sum
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int needed = target - nums[i];
-
-            if (map.find(needed) != map.end()) {
-                return {map[needed], i};
-            }
-
-            map[nums[i]] = i;
+        int n = nums.size();
+        
+        // 1. Pair values with their original indices
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < n; i++) {
+            numsWithIndex.push_back({nums[i], i});
         }
+        
+        // 2. Sort based on values
+        sort(numsWithIndex.begin(), numsWithIndex.end());
 
+        // 3. Two pointers
+        int left = 0, right = n - 1;
+        
+        while (left < right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            
+            if (sum == target) {
+                return {numsWithIndex[left].second, numsWithIndex[right].second};
+            } 
+            else if (sum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+        
         return {};
     }
 };
 
 int main() {
-    Solution solution;
+    Solution sol;
     
-    // Test case
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
+    vector<int> nums = {3, 2, 4};
+    int target = 6;
 
-    vector<int> result = solution.twoSum(nums, target);
+    vector<int> result = sol.twoSum(nums, target);
 
-    // Print the result
     if (!result.empty()) {
         cout << "Indices found: [" << result[0] << ", " << result[1] << "]" << endl;
     } else {
-        cout << "No two sum solution found." << endl;
+        cout << "No solution found." << endl;
     }
 
     return 0;
 }
+
+// // two sum
+// #include <iostream>
+
+// #include <vector>
+// #include <unordered_map>
+
+// using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> map;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             int needed = target - nums[i];
+
+//             if (map.find(needed) != map.end()) {
+//                 return {map[needed], i};
+//             }
+
+//             map[nums[i]] = i;
+//         }
+
+//         return {};
+//     }
+// };
+
+// int main() {
+//     Solution solution;
+    
+//     // Test case
+//     vector<int> nums = {2, 7, 11, 15};
+//     int target = 9;
+
+//     vector<int> result = solution.twoSum(nums, target);
+
+//     // Print the result
+//     if (!result.empty()) {
+//         cout << "Indices found: [" << result[0] << ", " << result[1] << "]" << endl;
+//     } else {
+//         cout << "No two sum solution found." << endl;
+//     }
+
+//     return 0;
+// }
