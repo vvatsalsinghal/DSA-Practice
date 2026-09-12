71ace586bdde7fdd5b462eb574616d4c79058ca0
vatsal <vatsallsinghal@gmail.com>
2026-09-12 21:27:17 +0530

    m11 73lc

---
+++ array/M11.cpp
@@ -0,0 +1,54 @@
+// 73 leetcode set matrix zeroes
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<bool> rowZero(m, false);
+        vector<bool> colZero(n, false);
+
+        // Step 1: Find all rows and columns containing 0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+
+        // Step 2: Set corresponding rows and columns to 0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rowZero[i] || colZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}
+    };
+
+    Solution obj;
+    obj.setZeroes(matrix);
+
+    // Print the matrix
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
