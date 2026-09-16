15a67c57a0e1fc3ea9bd4bc4e65db5f655bb4be8
vatsal <vatsallsinghal@gmail.com>
2026-09-16 22:50:07 +0530

    E2bs lc35

---
+++ binarysearch/E2.cpp
@@ -0,0 +1,40 @@
+// lc35
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st = 0, end = nums.size() - 1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (target > nums[mid]) {
+                st = mid + 1;
+            }
+            else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+
+        return st;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 2;
+
+    int result = sol.search(nums, target);
+
+    cout << result << endl;
+
+    return 0;
+}
